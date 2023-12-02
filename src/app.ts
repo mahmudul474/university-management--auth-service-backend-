820e18df6728369500face23ae958936159a2040
mahmudul474 <mdmahmudulla474@gmail.com>
2023-12-02 15:36:23 +0600

    user  interface &  model

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing  example
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("defould route to be  working successfully");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('defould route to be  working successfully')
+  next()
+})
 
-  next();
-});
-
-export default app;
+export default app
